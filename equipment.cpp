69a8d8153f0bb8c20f9f8a95ca9330ab31f9da7f
KreshnikuGreen <dion.deromemaj@protonmail.com>
2026-06-22 02:13:39 +0200

    Added fire() and reload() to equipment.cpp, most likely will have to revise

--- equipment.cpp
+++ equipment.cpp
@@ -1,2 +1,33 @@
 #include "equipment.h"
 #include <iostream>
+
+void Infantry_Ranged_Weapon::reload(){
+    if(Magazines <= 0){
+        Magazines = 0;// Doing both cases at once to reduce how much I have to write
+        std::cout << "Can't reload, no magazines left\n";
+    }
+
+    if(Ammo == 0){
+        Ammo = Max_Ammo - 1;
+    } else {
+        Ammo = Max_Ammo; // This is because Max_Ammo includes the round in the chamber, thus the initial -1 above
+    }
+}
+
+void Infantry_Ranged_Weapon::fire(){
+    int Rounds;
+
+    if(Ammo == 0){
+        std::cout << "Can't fire, no ammo left\n";
+    }
+
+    std::cout << "How many rounds do you want to fire? ";
+    std::cin >> Rounds;
+
+    if(Ammo < Rounds){
+        std::cout << "Can't fire, only " << Ammo << " rounds left\n";
+    } else {
+        Ammo -= Rounds;
+        std::cout << "FIRE!!! " << Rounds << " fired\n";
+    }
+}
