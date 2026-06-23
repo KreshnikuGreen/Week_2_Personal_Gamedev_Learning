0a3cf95cf3e9339025fb32524a0b8d06603198e9
KreshnikuGreen <dion.deromemaj@protonmail.com>
2026-06-24 01:03:47 +0200

    Added a soldier struct that gets partially randomly generated and random_utils to help with random number generation

---
+++ random_utils.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <random>
+
+inline int random_int(int min, int max){
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+
+    std::uniform_int_distribution<int> dist(min, max);
+    return dist(gen);
+}
--- soldier.h
+++ soldier.h
@@ -1,6 +1,49 @@
 #pragma once
+#include "equipment.h"
+#include "random_utils.h"
+#include <string>
 
 struct Soldier{
-    int maxHealth;
-    
+    const int   MAX_HEALTH;
+    const int   OCCUPATION;
+    /*
+     *  1 - Rifleman (if rank == 1)
+     *  2 - Designated_Marksman (if rank == 2)
+     *  3 - Grenadier (if rank == 2)
+     *  4 - Team Leader (if rank == 2)
+     *  5 - Medic/Repairman (if rank == 2)
+     *  6 - Team Leader (if rank == 3)
+     */
+    const int   RANK;
+    /*
+     * 1 - Normal
+     * 2 - Specialist
+     * 3 - Team Leader
+     */
+    bool        Alive;
+    /*
+     * True     = Alive
+     * False    = Dead
+     */
+    std::string Name;
+
+    Soldier() : 
+        MAX_HEALTH(100), 
+        RANK(random_int(1, 3)),
+        OCCUPATION(occupation_for_rank(RANK)),
+        Alive(true),
+        Name("Unnamed")
+    {}
+
+    private:
+
+    static int occupation_for_rank(int rank){
+        if(rank == 1){
+            return 1;
+        } else if(rank == 2){
+            return random_int(2, 5);
+        } else {
+            return 6;
+        }
+    }
 };
